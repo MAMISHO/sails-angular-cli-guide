d4de66d83a6a3edb661fc058f77bedb6b4432c14
Eduardo Pineda <sepineda@gmail.com>
2018-02-13 21:16:02 -0600

    Added wedding components

--- assets/app/app/app.module.ts
+++ assets/app/app/app.module.ts
@@ -4,18 +4,22 @@ import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { WhenWhereComponent } from './when-where/when-where.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OurStoryComponent,
+    WhenWhereComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'our-story', pathMatch: 'full' },
-      // { path: 'our-story', component: OurStoryComponent },
-      // { path: 'when-where', component: WhenWhereComponent }
+      { path: 'our-story', component: OurStoryComponent },
+      { path: 'when-where', component: WhenWhereComponent }
     ]),
     MaterializeModule
   ],
