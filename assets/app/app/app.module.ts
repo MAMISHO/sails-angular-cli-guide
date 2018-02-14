4d13c07d1c96b04b6295d7302a2c981e8094c10b
Eduardo Pineda <sepineda@gmail.com>
2018-02-13 18:54:38 -0600

    Added Materialize in client side

--- assets/app/app/app.module.ts
+++ assets/app/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
@@ -11,6 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'our-story', pathMatch: 'full' },
+      // { path: 'our-story', component: OurStoryComponent },
+      // { path: 'when-where', component: WhenWhereComponent }
+    ]),
     MaterializeModule
   ],
   providers: [],
